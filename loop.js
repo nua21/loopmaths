e909da1343b12d226f72d4f3eb02da7d8301427f
nua21 <nusrafffe143@gmail.com>
2024-10-17 20:09:44 +0600

    Updated loop

--- loop.js
+++ loop.js
@@ -53,7 +53,7 @@ sum = sum + Math.pow(i,2);
 let number = 10;
 let number5 = 7;
 for(let i = 1; i <= number; i++){
-    console.log(i * number5);    
+    console.log(number5 + " " + "X" + " " + i + "=" + " " + number5 * i );    
 }
 // PROBLEM 5 SOLVED
 
